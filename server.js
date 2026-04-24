acada0d7c2fe56b6a9c349dd16e95c3f802a690d
TIT <thaodc24v7k028@vlvh.ctu.edu.vn>
2026-04-24 21:09:57 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
 async function startServer() {
-    try {
-        await MongoDB.connect(config.db.uri);
-        console.log("Connect to the database ! hihihi");
-        const PORT = config.app.port;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }
-    catch (error) {
-        console.log("Cannot connect to the database!", error);
-        process.exit();
-    }
-
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connect to the database ! hihihi");
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Cannot connect to the database!", error);
+    process.exit();
+  }
 }
 
 startServer();
